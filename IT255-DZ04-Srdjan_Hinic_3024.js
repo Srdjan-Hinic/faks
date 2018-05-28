533175111bf40ec69d445105b0068624ad091dd9
Srdjan-Hinic <36701956+Srdjan-Hinic@users.noreply.github.com>
2018-05-28 09:23:21 +0200

    Add files via upload

---
+++ IT255-DZ04-Srdjan_Hinic_3024.js
@@ -0,0 +1,48 @@
+function colors(n) {
+    switch (n) {
+        case 1:
+            document.body.style.backgroundColor = "white";
+            document.getElementById("head1").style.background = "green";
+            document.getElementById("bodybox").style.background = "white";
+            document.getElementById("leftbox").style.background = "white";
+            document.getElementById("rightbox").style.background = "white";
+            document.body.style.color = "black";
+            document.getElementById("leftlink1").style.color = "black";
+            document.getElementById("leftlink2").style.color = "black";
+            document.getElementById("leftlink3").style.color = "black";
+            document.getElementById("leftlink4").style.color = "black";
+            document.getElementById("leftlink5").style.color = "black";
+            document.getElementById("leftlink6").style.color = "black";
+            break;
+        case 2:
+            document.body.style.backgroundColor = "yellow";
+            document.getElementById("head1").style.background = "blue";
+            document.getElementById("bodybox").style.background = "yellow";
+            document.getElementById("leftbox").style.background = "yellow";
+            document.getElementById("rightbox").style.background = "yellow";
+            document.body.style.color = "black"
+            document.getElementById("leftlink1").style.color = "black";
+            document.getElementById("leftlink2").style.color = "black";
+            document.getElementById("leftlink3").style.color = "black";
+            document.getElementById("leftlink4").style.color = "black";
+            document.getElementById("leftlink5").style.color = "black";
+            document.getElementById("leftlink6").style.color = "black";
+            break;
+        case 3:
+            document.body.style.backgroundColor = "gray";
+            document.getElementById("head1").style.background = "black";
+            document.getElementById("bodybox").style.background = "gray";
+            document.getElementById("leftbox").style.background = "gray";
+            document.getElementById("rightbox").style.background = "gray";
+            document.body.style.color = "white";
+            document.getElementById("leftlink1").style.color = "white";
+            document.getElementById("leftlink2").style.color = "white";
+            document.getElementById("leftlink3").style.color = "white";
+            document.getElementById("leftlink4").style.color = "white";
+            document.getElementById("leftlink5").style.color = "white";
+            document.getElementById("leftlink6").style.color = "white";
+            
+            break;
+
+    }
+}
